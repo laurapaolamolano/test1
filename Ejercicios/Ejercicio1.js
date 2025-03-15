1b5abcc0b5a1a38b31dd57b68767e4ca63a2f987
LauraMolano <laurapaola.molano172@comunidadunir.net>
2025-03-14 23:55:19 -0500

    Actualización otra vez

--- Ejercicios/Ejercicio1.js
+++ Ejercicios/Ejercicio1.js
@@ -1,3 +1,4 @@
 console.log("Hello World!");
-console.log("Este es un mensaje");
-console.log("Este es otro mensaje");
+console.log("Este es un mensaje de prueba 1");
+console.log("Este es un mensaje de prueba 2");
+console.log("Este es un mensaje de prueba 3");
